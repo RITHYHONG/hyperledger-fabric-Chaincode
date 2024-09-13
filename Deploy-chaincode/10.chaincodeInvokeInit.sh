8da74724883153549f9cd127c4143d9d12ee63a2
RITHYHONG <rithyhong8998@gmail.com>
2024-09-13 16:46:54 +0700

    Hello

--- Deploy-chaincode/10.chaincodeInvokeInit.sh
+++ Deploy-chaincode/10.chaincodeInvokeInit.sh
@@ -8,7 +8,7 @@ export CORE_PEER_ADDRESS=localhost:7051
 
 peer chaincode invoke -o localhost:7050 \
      --tls --cafile "${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
-     -C channel -n mycc \
+     --channelID channel -n mycc \
      --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/../crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
      --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/../crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
      -c '{"function":"InitLedger","Args":[]}' --isInit 
--- chaincode-go/main.go
+++ chaincode-go/main.go
@@ -1,42 +1,97 @@
 package main
 
 import (
-    "fmt"
-    "github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 type SmartContract struct {
-    contractapi.Contract
+	contractapi.Contract
 }
 
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, value string) error {
-    exists, err := s.AssetExists(ctx, id)
-    if err != nil {
-        return fmt.Errorf("failed to check if asset exists: %s", err.Error())
-    }
-    if exists {
-        return fmt.Errorf("asset %s already exists", id)
-    }
-
-    return ctx.GetStub().PutState(id, []byte(value))
+type Car struct {
+	Make   string `json:"make"`
+	Model  string `json:"model"`
+	Colour string `json:"colour"`
+	Owner  string `json:"owner"`
 }
 
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-    assetBytes, err := ctx.GetStub().GetState(id)
-    if err != nil {
-        return false, fmt.Errorf("failed to read from world state: %s", err.Error())
-    }
-    return assetBytes != nil, nil
+type QueryResult struct {
+	Key    string `json:"Key"`
+	Record *Car
 }
+
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	cars := []Car{
+		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
+		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
+		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
+		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
+		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
+		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
+		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
+		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
+		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
+		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+	}
+
+	for i, car := range cars {
+		carAsBytes, _ := json.Marshal(car)
+		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+
+		if err != nil {
+			return fmt.Errorf("Failed to put to world state. %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+
+func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+	startKey := "CAR0"
+	endKey := "CAR99"
+
+	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	results := []QueryResult{}
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+
+		if err != nil {
+			return nil, err
+		}
+
+		car := new(Car)
+		_ = json.Unmarshal(queryResponse.Value, car)
+
+		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
+		results = append(results, queryResult)
+	}
+
+	return results, nil
+}
+
 
 func main() {
-    chaincode, err := contractapi.NewChaincode(new(SmartContract))
-    if err != nil {
-        fmt.Printf("Error creating chaincode: %s", err.Error())
-        return
-    }
 
-    if err := chaincode.Start(); err != nil {
-        fmt.Printf("Error starting chaincode: %s", err.Error())
-    }
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+
+	if err != nil {
+		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+	}
 }
